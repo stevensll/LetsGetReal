9b0c3049ee5a84b378c83c14070403c615406873
Steven <deltaorb637@gmail.com>
2020-11-19 14:26:41 -0500

    Created RationalNumber, RealNumber classes. Added baseline methods.

---
+++ RationalNumber.java
@@ -0,0 +1,20 @@
+public class RationalNumber extends RealNumber{
+    private int numerator, denominator;
+
+    public RationalNumber(int nume, int deno){
+        super(0.0);
+    }
+
+    public double getValue(){
+        return 0.0;
+    }
+
+    public int getNumerator(){
+        return numerator;
+    }
+
+    public int getDenominator(){
+        return denominator;
+    }
+
+}
---
+++ RealNumber.java
@@ -0,0 +1,16 @@
+public class RealNumber{
+    private double value;
+
+    public RealNumber(double v){
+        value  = v;
+    }
+
+    public double getValue(){
+        return value;
+    }
+
+    public String toString(){
+        return ""+getValue();
+    }
+
+}
